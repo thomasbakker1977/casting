67cad6290c9386b8c320b8c08c89db82783ab1fc
thomasbakker1977 <113938575+thomasbakker1977@users.noreply.github.com>
2023-01-17 12:22:24 +0100

    Added parts 1-4 to main.py

--- main.py
+++ main.py
@@ -6,4 +6,14 @@ __human_name__ = 'casting'
 leek_price = 2
 leek_price = str(leek_price)
 
-print ("Leek is " + leek_price + " euro per kilo.")
+leek_string = ("Leek is " + leek_price + " euro per kilo.")
+print (leek_string)
+
+
+#Use find and slicing to extract the number from this string.
+print(leek_string[8:9]) #24
+
+#Cast it into an integer.
+int_leek_price = int(leek_price)
+
+#Use this and leek_price to compute the sum total and store it in sum_total. Print out the value for this variable.
