2eb7d6ece0606e8ea855cd5d5352fc1e9cb7d6bc
thomasbakker1977 <113938575+thomasbakker1977@users.noreply.github.com>
2023-01-21 19:10:49 +0100

    Added the solution for this assignment

--- main.py
+++ main.py
@@ -10,10 +10,27 @@ print ("Leek is " + leek_price + " euro per kilo.")
 
 
 #We've got an order of 4 leeks
-leek_4 = (int(leek_price) *4)
+leek_4 = ('We got an order for 4 leeks')
 
 #Use find and slicing to extract the number from this string.
-leek4_find.find()
+find_leek_four = leek_4.find('four')
+
+
+#Cast it into an integer.
+int(find_leek_four)
+
+#Use this and leek_price to compute the sum total and store it in sum_total. Print out the value #for this variable.
+sum_total = (int(leek_price) * 4)
+print (sum_total)
+
+#Broccoli costs 2.34 euros per kg. We got an order: 'broccoli 1.6'.
+
+price_broccoli = (2.34)
+order_broccoli = (1.6)
+
+total_price = (price_broccoli *order_broccoli)
+
+print ('1.6kg broccoli costs ' + str(round(total_price,2))+'e')
 
 
 
